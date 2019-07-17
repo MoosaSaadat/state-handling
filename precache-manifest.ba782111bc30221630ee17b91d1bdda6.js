8f025192c86a62723d766a0ef7618614d08a8a4c
Muhammad Musa <moosasaadat99@gmail.com>
2019-07-17 12:57:19 +0500

    Updates

---
+++ precache-manifest.ba782111bc30221630ee17b91d1bdda6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b554e436659abd13efef9e09da8f29a",
+    "url": "/state-handling/index.html"
+  },
+  {
+    "revision": "57aa0a20638c933f1ec1",
+    "url": "/state-handling/static/css/main.b536fc04.chunk.css"
+  },
+  {
+    "revision": "d36319d4426d9cdf8db3",
+    "url": "/state-handling/static/js/2.ea4eac72.chunk.js"
+  },
+  {
+    "revision": "57aa0a20638c933f1ec1",
+    "url": "/state-handling/static/js/main.9cce7cbe.chunk.js"
+  },
+  {
+    "revision": "694b1d286c5e482f93b7",
+    "url": "/state-handling/static/js/runtime~main.268e8d49.js"
+  },
+  {
+    "revision": "db9097e874e68b7ed7115ff05ffcb880",
+    "url": "/state-handling/static/media/0.db9097e8.jpg"
+  },
+  {
+    "revision": "fbff4784004cc97463c883477a0090d1",
+    "url": "/state-handling/static/media/1.fbff4784.jpg"
+  },
+  {
+    "revision": "c4ea9528b67823bd94c90de3d97d75a3",
+    "url": "/state-handling/static/media/2.c4ea9528.jpg"
+  },
+  {
+    "revision": "62a781c89c590abb27129638d9d18c4d",
+    "url": "/state-handling/static/media/3.62a781c8.jpg"
+  },
+  {
+    "revision": "0f8ca81ee9f7b2bc27102267f9e23864",
+    "url": "/state-handling/static/media/4.0f8ca81e.jpg"
+  },
+  {
+    "revision": "1b557aa7ccfa88b6b3f4723c27d3f2d6",
+    "url": "/state-handling/static/media/5.1b557aa7.jpg"
+  },
+  {
+    "revision": "c80394c2a96ffad09a9df4e4815745f2",
+    "url": "/state-handling/static/media/6.c80394c2.jpg"
+  }
+]);
